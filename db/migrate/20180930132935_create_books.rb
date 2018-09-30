0072703b1b9d25a7696ed7f16f4135c0d2b746c5
Vu pt <vuit6789@gmail.com>
2018-09-30 20:36:48 +0700

    Initial model

---
+++ db/migrate/20180930132935_create_books.rb
@@ -0,0 +1,18 @@
+class CreateBooks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :description
+      t.string :author
+      t.integer :rates
+      t.integer :category_id
+      t.string :file
+      t.string :book_img_file_name
+      t.integer :book_img_file_size
+      t.string :book_img_content_type
+      t.datetime :book_img_updated_at
+
+      t.timestamps
+    end
+  end
+end
