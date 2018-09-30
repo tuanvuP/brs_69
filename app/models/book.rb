1bc4362b29a77b9c5559a3eb78fdb5873245babf
Vu pt <vuit6789@gmail.com>
2018-10-01 01:32:47 +0700

    Add book RESTful

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,15 @@
 class Book < ApplicationRecord
-  has_many :reviews, dependent: :destroy
-  has_many :book_status
-  belongs_to :user
+  # has_many :reviews, dependent: :destroy
+  # has_many :book_status
+  # belongs_to :user
   belongs_to :category
+
+  validates :title, presence: true,
+    length: {maximum: Settings.models.book.title_max_len}
+  validates :author,
+    length: {maximum: Settings.models.book.author_name_max_len}
+  validates :description,
+    length: {maximum: Settings.models.book.descrip_max_len}
+
+  scope :sort_by_desc, ->{order(created_at: :desc)}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :books
-  has_many :reviews
-  has_many :comments
-  has_many :requests
-  has_many :book_statuses
+  # has_many :books
+  # has_many :reviews
+  # has_many :comments
+  # has_many :requests
+  # has_many :book_statuses
 end
