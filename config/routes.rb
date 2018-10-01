9dcaca9fa943b4061413920bc60749d5746dfc3e
hoaithuan <hoaithuan95@gmail.com>
2018-10-01 09:02:02 +0700

    Initial Project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   root "static_pages#home"
-  get "/signup", to: "users#new"
+  get "sessions/new"
   get "users/new"
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   resources :users
 end
