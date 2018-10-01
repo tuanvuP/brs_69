ed03e4d271b1d84189150e9bc4e668fdb438863f
hoaithuan <hoaithuan95@gmail.com>
2018-10-01 09:02:02 +0700

    Refactoring code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,21 @@
 class User < ApplicationRecord
-
-  before_save { self.email = email.downcase }
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :name, presence: true, length: {maximum: Settings.user.name.max_size}
-  validates :email, presence: true, length: {maximum: Settings.user.email.max_size},
-   format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: {minimum: Settings.user.password.min_size}
-
-  has_secure_password
 
   has_many :books
   has_many :reviews
   has_many :comments
   has_many :requests
   has_many :book_status
+
+  validates :name, presence: true,
+    length: {maximum: Settings.models.user.name.max_size}
+  validates :email, presence: true,
+    length: {maximum: Settings.models.user.email.max_size},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+  validates :password, presence: true,
+    length: {minimum: Settings.models.user.password.min_size}
+
+  has_secure_password
+
+  before_save {email.downcase!}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_10_01_041328) do
 
   create_table "book_statuses", force: :cascade do |t|
