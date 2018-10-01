a1b35b1b20c2cb02fe7f957f08f0055dae319bc0
hoaithuan <hoaithuan95@gmail.com>
2018-10-01 09:02:02 +0700

    Initial Project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+
   def full_title page_title = ""
     base_title = I18n.t(".base_title")
     page_title.blank? ? base_title : page_title + " | " + base_title
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+
+  before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: {maximum: Settings.user.name.max_size}
+  validates :email, presence: true, length: {maximum: Settings.user.email.max_size},
+   format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: {minimum: Settings.user.password.min_size}
+
+  has_secure_password
+
   has_many :books
   has_many :reviews
   has_many :comments
