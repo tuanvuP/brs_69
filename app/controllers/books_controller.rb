d0db23485daac90bb4eba1ca7e6ea8ffe3167b8a
hoaithuan <hoaithuan95@gmail.com>
2018-10-01 09:02:02 +0700

    Initial Project

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,74 @@
+class BooksController < ApplicationController
+  before_action :load_book, except: [:index, :new, :create]
+  before_action :load_category, only: [:edit, :new, :create]
+
+  def index
+    if params[:category].blank?
+      @books = Book.sort_by_desc.paginate page: params[:page],
+        per_page: Settings.controller.book.per_page
+    elsif Category.find_by(name: params[:category]).present?
+      @category = Category.find_by name: params[:category]
+      @books = @category.books.sort_by_desc
+        .paginate page: params[:page], per_page: Settings.controller.book.per_page
+    else
+      render file: "public/404.html", status: :not_found, layout: false
+    end
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def show; end
+
+  def destroy
+    if @book.destroy
+      flash[:success] = t "delete_successfully"
+    else
+      flash.now[:danger] = t "error_message"
+    end
+    redirect_to books_url
+  end
+
+  def update
+    @book.category_id = params[:category_id]
+
+    if @book.update_attributes book_params
+      flash[:success] = t "update_successfully"
+      redirect_to @book
+    else
+      render :edit
+    end
+  end
+
+  def edit; end
+
+  def create
+    @book = Book.new book_params
+    @book.category_id = params[:category_id]
+
+    if @book.save
+      flash[:success] = t "create_successfully"
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit :title, :description, :author, :category_id,
+      :book_img_file_name
+  end
+
+  def load_book
+    @book = Book.find_by id: params[:id]
+    return if @book
+    render file: "public/404.html", status: :not_found, layout: false
+  end
+
+  def load_category
+    @categories = Category.all.map{ |m| [m.name, m.id] }
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,27 @@
-User.create!(name: "aaa", email: "a@gmail.com", password: "123123",
-  password_confirmation: "123123", role: 1)
-
-77.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name: name, email: email, password: password,
-    password_confirmation: password, role: 0, activated: true)
-end
-
 users = User.all
 user = users.first
 following = users[2..50]
 followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
+
+User.create!(name: "Admin",
+             email: "admin@admin.com",
+             password: "admin123",
+             password_confirmation: "admin123",
+             role: 1)
+
+50.times do |n|
+  name = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+3.times do |n|
+  name = Faker::Book.genre
+  Category.create!(name: name)
+end
