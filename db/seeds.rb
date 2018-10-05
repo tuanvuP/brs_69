66e7a6b4001a15b60a543032b1834c6b19a74050
Vu pt <vuit6789@gmail.com>
2018-10-05 17:45:49 +0700

    manager books

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
-User.create!(name: "aaa", email: "a@gmail.com", password: "123123",
-  password_confirmation: "123123", role: 1)
+User.create!(name: "Admin",
+             email: "admin@admin.com",
+             password: "admin123",
+             password_confirmation: "admin123",
+             role: 1)
 
-77.times do |n|
-  name  = Faker::Name.name
+50.times do |n|
+  name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name: name, email: email, password: password,
-    password_confirmation: password, role: 0, activated: true)
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
+3.times do |n|
+  name = Faker::Book.genre
+  Category.create!(name: name)
 end
 
 users = User.all
@@ -15,3 +25,14 @@ following = users[2..50]
 followers = users[3..40]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
+
+99.times do |n|
+  title = Faker::Book.title
+  description = "Book's description-#{n+1}"
+  author = Faker::Book.author
+  category_id = [1, 2, 3].sample
+  Book.create!(title: title,
+               description: description,
+               author: author,
+               category_id: category_id)
+end
